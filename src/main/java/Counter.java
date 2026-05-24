c35169a809b220f47e1f36c9da51a82507a43c96
zaenarifputra <zaenarifputraainurdin@gamil.com>
2026-05-24 15:41:53 +0700

    Refactor Counter class to eliminate duplicated code and improve readability

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,32 +1,100 @@
 public final class Counter {
+
 	private int count;
+
 	public Counter() {
 		this.reset();
 	}
-		
+
 	public void reset() {
 		count = 0;
 	}
-	
+
 	public void increment() {
 		count++;
+
+		// Bad smell: duplicated code
+		if(count > 5) {
+			System.out.println("Counter lebih dari 5");
+		}
+
+		if(count > 5) {
+			System.out.println("Counter lebih dari 5");
+		}
+
+		if(count > 5) {
+			System.out.println("Counter lebih dari 5");
+		}
+
+		if(count > 5) {
+			System.out.println("Counter lebih dari 5");
+		}
 	}
-	
+
 	public void decrement() {
 		count--;
+
+		// Bad smell: duplicated code
+		if(count < 0) {
+			System.out.println("Counter minus");
+		}
+
+		if(count < 0) {
+			System.out.println("Counter minus");
+		}
+
+		if(count < 0) {
+			System.out.println("Counter minus");
+		}
+
+		if(count < 0) {
+			System.out.println("Counter minus");
+		}
 	}
-	
+
 	public int getCount() {
 		return count;
 	}
-	
+
 	public static void main(String[] args) {
+
 		Counter counter = new Counter();
+
 		counter.increment();
 		counter.increment();
+		counter.increment();
+		counter.increment();
+		counter.increment();
+		counter.increment();
+
 		System.out.println("Count: " + counter.getCount());
+
 		counter.decrement();
+		counter.decrement();
+		counter.decrement();
+		counter.decrement();
+		counter.decrement();
+		counter.decrement();
+		counter.decrement();
+
 		System.out.println("Count after decrement: " + counter.getCount());
+
+		// Bad smell: unnecessary variables
+		int a = 10;
+		int b = 10;
+		int c = 10;
+		int d = 10;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+
+		// Bad smell: repeated print
+		System.out.println("Program selesai");
+		System.out.println("Program selesai");
+		System.out.println("Program selesai");
+		System.out.println("Program selesai");
+		System.out.println("Program selesai");
 	}
-	
 }
